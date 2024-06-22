f0b6b4e09c4c422bdba1d1d4f2ea87c565367c19
Binod Rasaili <binod.toyam@gmail.com>
2024-06-22 00:05:17 -0500

    api gateway working

--- config-server/src/main/java/com/binode/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/binode/configserver/ConfigServerApplication.java
@@ -1,8 +1,11 @@
 package com.binode.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
